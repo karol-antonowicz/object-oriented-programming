8bed714da7d944771e7bce41ef86a6d47c07f9bc
Karol Antonowicz <karol.antonowicz@gmail.com>
2019-10-25 10:32:11 +0200

    initial commit: created script for masking numbers and adding to html

---
+++ masking/test.js
@@ -0,0 +1,82 @@
+// lsita osob w raz z numerami kont bankowych
+// full name i account umber
+
+
+class Person {
+    constructor(name, number) {
+        this._name = name;
+        this._number = number;
+    }
+    get name() {
+        return this._name;
+    }
+    set name(newName) {
+        this._name = newName
+    }
+    get number() {
+        return this._number
+    }
+    set number(number) {
+        return this._number
+
+    }
+}
+
+// creating new persons using Person clas
+
+let person1 = new Person('Michal', '21458154914231425142540004')
+let person2 = new Person('Karol', '12145815491423142514254784')
+
+
+
+//function number - HIDING NUMBERS
+
+function masking(number) {
+let a = number.split("")                                     // rozdzielam do tablicy gdzie kazda cyfra zajmuje miejsce wtabeli
+var lol = [];                                           // tworze pusta tabele
+for (i = 2; i < (number.length - 4); i++)           // dla kazdego elementu tablicy 
+lol.push('#')
+var masked = lol.join("");
+return number[0]+number[1]+masked + number.slice(-4); 
+
+}
+/// array of persons with data
+
+var personList = [];
+personList.push(person1,person2)
+
+var personNumbers = [];
+personNumbers.push(person1.name, masking(person1.number),person2.name, masking(person2.number))
+
+//DOM 
+//declaring values
+
+divDisplay = document.getElementById('display')         // grabs the div
+
+let makeSectionT = document.createElement('section')    // creates section tag 
+                                                          // grabs the button #showButton
+
+// empty array
+
+var database = [];
+
+// kalsa Render sluzy do mmetody wyrenderowania
+
+
+
+
+var output = [];                                                    // output gdzie wrzuce numer konts
+                                                                     // poutpu ze studentami
+
+let a = personNumbers.forEach((x => output +=`<div>${x}</div>`))      //maps the teachersList for every element in the studentsList table it adds this line to output
+
+let makeSection = document.createElement('section')  
+divDisplay.appendChild(makeSection).classList.add('account')              // creates section element in DOM
+
+document.getElementsByClassName('account')[0].innerHTML = output 
+
+
+
+
+
+
